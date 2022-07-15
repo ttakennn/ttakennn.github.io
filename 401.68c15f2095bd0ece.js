96dc8113054f31c11a9ee9a7818ca2fe01747a5d
KEN <ttakennn@gmail.com>
2022-07-15 22:51:25 +0700

    re-update wedding page

--- 401.68c15f2095bd0ece.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweb_app=self.webpackChunkweb_app||[]).push([[401],{8401:(n,a,o)=>{o.r(a),o.d(a,{LayoutModule:()=>l});var t=o(1165),e=o(5e3);let l=(()=>{class u{}return u.\u0275fac=function(d){return new(d||u)},u.\u0275mod=e.oAB({type:u}),u.\u0275inj=e.cJS({imports:[[t.Z]]}),u})()}}]);
